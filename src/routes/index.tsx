822b37ef68975f6740beadbf6c5f354ac2e52041
Jonas <publik.froeller.jonas@gmx.at>
2023-06-23 21:26:44 +0200

    fixed broken links

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,22 +9,22 @@ export default component$(() => {
       <Hero />
 
       <section class="container container-links container-flex container-spacing-xl">
-        <a class="button button-dark" href="https://jonasfroeller.vercel.app" target='_blank'><img src="/globe.svg" color="white" alt="" /> Portfolio</a>
-        <a class="button button-dark" href="https://github.com/jonasfroeller" target='_blank'><img src="/github.svg" color="white" alt="" /> GitHub</a>
-        <a class="button button-dark" href="https://gist.github.com/jonasfroeller" target='_blank'><img src="/file.svg" color="white" alt="" /> GitHub Gists</a>
-        <a class="button button-dark" href="https://webdev-snippets.pages.dev/" target='_blank'><img src="/code.svg" color="white" alt="" /> Code Snippets</a>
-        <a class="button button-dark" href="https://vsc-code-snippets.vercel.app/ target='_blank'"><img src="/code.svg" color="white" alt="" /> VSC Snippets</a>
-        <a class="button button-dark" href="https://svelte-kit-markdown-blog.vercel.app/ target='_blank'"><img src="/align-right.svg" color="white" alt="" /> Blog</a>
-        <a class="button button-dark" href="https://codepen.io/jonasfroeller" target='_blank'><img src="/codepen.svg" color="white" alt="" /> CodePen</a>
-        <a class="button button-dark" href="https://www.npmjs.com/~jonasfroeller" target='_blank'><img src="/external-link.svg" color="white" alt="" /> NPM</a>
-        <a class="button button-dark" href="https://pixabay.com/de/users/iproduceassets-30997310/" target='_blank'><img src="/image.svg" color="white" alt="" /> Pixabay</a>
-        <a class="button button-dark" href="https://www.xing.com/profile/Jonas_Froeller/cv" target='_blank'><img src="/external-link.svg" color="white" alt="" /> Xing</a>
-        <a class="button button-dark" href="https://www.linkedin.com/in/jonas-fr%C3%B6ller-8b4986235/" target='_blank'><img src="/linkedin.svg" color="white" alt="" /> LinkedIn</a>
-        <a class="button button-dark" href="https://stackoverflow.com/users/17421351/easyoutcoded" target='_blank'><img src="/layers.svg" color="white" alt="" /> StackOverflow</a>
-        <a class="button button-dark" href="https://hub.docker.com/u/jonasfroeller" target='_blank'><img src="/external-link.svg" color="white" alt="" /> DockerHub</a>
-        <a class="button button-dark" href="https://www.fiverr.com/codedesignmedia" target='_blank'><img src="/layout.svg" color="white" alt="" /> Fiverr</a>
-        <a class="button button-dark" href="https://www.figma.com/@jonasfroeller" target='_blank'><img src="/figma.svg" color="white" alt="" /> Figma</a>
-        <a class="button button-dark" href="https://dev.to/jonasfroeller" target='_blank'><img src="/align-right.svg" color="white" alt="" /> DevTo</a>
+        <a class="button button-dark" href="https://jonasfroeller.vercel.app" target='_blank'><img src="/globe.svg" color="white" alt="globe" /> Portfolio</a>
+        <a class="button button-dark" href="https://github.com/jonasfroeller" target='_blank'><img src="/github.svg" color="white" alt="github" /> GitHub</a>
+        <a class="button button-dark" href="https://gist.github.com/jonasfroeller" target='_blank'><img src="/file.svg" color="white" alt="file" /> GitHub Gists</a>
+        <a class="button button-dark" href="https://webdev-snippets.pages.dev" target='_blank'><img src="/code.svg" color="white" alt="code" /> Code Snippets</a>
+        <a class="button button-dark" href="https://vsc-code-snippets.vercel.app" target='_blank'><img src="/code.svg" color="white" alt="code" /> VSC Snippets</a>
+        <a class="button button-dark" href="https://svelte-kit-markdown-blog.vercel.app" target='_blank'><img src="/align-right.svg" color="white" alt="align-right" /> Blog</a>
+        <a class="button button-dark" href="https://codepen.io/jonasfroeller" target='_blank'><img src="/codepen.svg" color="white" alt="codepen" /> CodePen</a>
+        <a class="button button-dark" href="https://www.npmjs.com/~jonasfroeller" target='_blank'><img src="/external-link.svg" color="white" alt="external-link" /> NPM</a>
+        <a class="button button-dark" href="https://pixabay.com/de/users/jonasfroeller-34602411" target='_blank'><img src="/image.svg" color="white" alt="image" /> Pixabay</a>
+        <a class="button button-dark" href="https://www.xing.com/profile/Jonas_Froeller/cv" target='_blank'><img src="/external-link.svg" color="white" alt="external-link" /> Xing</a>
+        <a class="button button-dark" href="https://www.linkedin.com/in/jonas-fr%C3%B6ller-8b4986235" target='_blank'><img src="/linkedin.svg" color="white" alt="linkedin" /> LinkedIn</a>
+        <a class="button button-dark" href="https://stackoverflow.com/users/17421351/easyoutcoded" target='_blank'><img src="/layers.svg" color="white" alt="layers" /> StackOverflow</a>
+        <a class="button button-dark" href="https://hub.docker.com/u/jonasfroeller" target='_blank'><img src="/external-link.svg" color="white" alt="external-link" /> DockerHub</a>
+        <a class="button button-dark" href="https://www.fiverr.com/codedesignmedia" target='_blank'><img src="/layout.svg" color="white" alt="layout" /> Fiverr</a>
+        <a class="button button-dark" href="https://www.figma.com/@jonasfroeller" target='_blank'><img src="/figma.svg" color="white" alt="figma" /> Figma</a>
+        <a class="button button-dark" href="https://dev.to/jonasfroeller" target='_blank'><img src="/align-right.svg" color="white" alt="align-right" /> DevTo</a>
       </section>
     </>
   );
